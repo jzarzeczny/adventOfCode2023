4e442aca5e84ddd7af320485cb0b3289e4fc58c0
Jakub Zarzeczny <jkzarzeczny@gmail.com>
2023-12-01 13:35:56 +0100

    [AOC-2023] Day 1 part 2

--- day1/index.js
+++ day1/index.js
@@ -11,23 +11,8 @@ async function main() {
   const calibrationValues = [];
 
   for await (const line of rl) {
-    const lineNumbers = [];
-    for (let i = 0; i < line.length; i++) {
-      const int = parseInt(line[i]);
-      if (int) {
-        lineNumbers.push(int);
-      }
-    }
-    if (lineNumbers.length === 0) {
-      return;
-    }
-    if (lineNumbers.length === 1) {
-      calibrationValues.push(parseInt(`${lineNumbers[0]}${lineNumbers[0]}`));
-    } else {
-      calibrationValues.push(
-        parseInt(`${lineNumbers[0]}${lineNumbers[lineNumbers.length - 1]}`)
-      );
-    }
+    const lineCalibration = findLineCalibration(line);
+    calibrationValues.push(lineCalibration);
   }
   const finalValue = calibrationValues.reduce((val, acc) => {
     return acc + val;
@@ -36,3 +21,59 @@ async function main() {
 }
 
 main();
+
+function findLineCalibration(line) {
+  const words = findWords(line);
+  const numbers = findNumbers(line);
+  const lineNumbers = Object.entries({ ...words, ...numbers }).map(
+    (pair) => pair[1]
+  );
+  if (lineNumbers.length === 0) {
+    return;
+  }
+  if (lineNumbers.length === 1) {
+    return parseInt(`${lineNumbers[0]}${lineNumbers[0]}`);
+  } else {
+    return parseInt(`${lineNumbers[0]}${lineNumbers[lineNumbers.length - 1]}`);
+  }
+}
+
+function findWords(line) {
+  const wordNumbersInLine = {};
+  const wordNumberObject = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  const wordNumbersArray = Object.keys(wordNumberObject);
+
+  for (let i = 0; i < wordNumbersArray.length; i++) {
+    const elementKey = [
+      ...line.matchAll(new RegExp(wordNumbersArray[i], "gi")),
+    ].map((e) => e.index);
+    elementKey.forEach((element) => {
+      wordNumbersInLine[element] = wordNumberObject[wordNumbersArray[i]];
+    });
+  }
+  return wordNumbersInLine;
+}
+
+function findNumbers(line) {
+  const numbersInLine = {};
+
+  for (let i = 0; i < line.length; i++) {
+    const int = parseInt(line[i]);
+    if (int) {
+      numbersInLine[i] = int;
+    }
+  }
+
+  return numbersInLine;
+}
